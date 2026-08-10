c50e54c2068d620589dd14fefccdfff5f2266a75
pragatipatil1428 <pragatipatil1428@gmail.com>
2026-08-10 20:28:42 +0530

    favicon updated

--- proxy.ts
+++ proxy.ts
@@ -6,10 +6,10 @@ export const config = {
      * Match all request paths except:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - favicon.ico, icon.svg (favicon files)
      * - login, register, forgot-password, verify-email (auth pages)
      * - api/auth (NextAuth API routes)
      */
-    "/((?!_next/static|_next/image|favicon.ico|login|register|forgot-password|verify-email|api/auth).*)",
+    "/((?!_next/static|_next/image|favicon.ico|icon.svg|login|register|forgot-password|verify-email|api/auth).*)",
   ],
 };
